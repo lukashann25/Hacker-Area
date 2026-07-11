253e709a006a9a48b197bfd2bcf6e61bf0c751d7
lukashann25 <lukashann45@gmail.com>
2026-07-11 20:06:34 +0200

    Refactor password validation and result display
    Refactor password checking logic and improve user feedback.

--- level4.js
+++ level4.js
@@ -1,44 +1,43 @@
-console.log("level4.js geladen");
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAeJWnxc_YMbBLAF1bCgj6bhXidkihlqRA",
-    authDomain: "hacker-area.firebaseapp.com",
-    projectId: "hacker-area",
-    storageBucket: "hacker-area.firebasestorage.app",
-    messagingSenderId: "150908413978",
-    appId: "1:150908413978:web:7c6c028a13c4bc58e309a3",
-    measurementId: "G-9KEXY5C5NQ" };
+  apiKey: "AIzaSyAeJWnxc_YMbBLAF1bCgj6bhXidkihlqRA",
+  authDomain: "hacker-area.firebaseapp.com",
+  projectId: "hacker-area",
+  storageBucket: "hacker-area.firebasestorage.app",
+  messagingSenderId: "150908413978",
+  appId: "1:150908413978:web:7c6c028a13c4bc58e309a3",
+  measurementId: "G-9KEXY5C5NQ"
+};
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+
 window.checkPassword = async function() {
-    console.log("Button gedrückt");
-    
-    const eingabe = document.getElementById("password").value;
 
-    console.log("Eingabe:", eingabe);
-    
-    const level4 = doc(db, "levels", "level4");
+  const eingabe = document.getElementById("password").value;
 
-    const daten = await getDoc(level4);
+  const level4 = doc(db, "levels", "level4");
 
-    if (daten.exists()) {
-        const richtigesPasswort = daten.data().password;
+  const daten = await getDoc(level4);
 
-        if (eingabe === richtigesPasswort) {
 
-document.getElementById("result").innerHTML = "Level geschafft!";
-            }else{
+  if (daten.exists()) {
 
-document.getElementById("result").innerHTML = "Falsches Passwort!";
-            }
+    const richtigesPasswort = daten.data().password;
 
-        } else {
-
-document.getElementById("result").innerHTML = "Level in Bearbeitung";
-        }
-
+    if (eingabe === richtigesPasswort) {
+      document.getElementById("result").innerHTML = "Level geschafft! 🎉";
+    } else {
+      document.getElementById("result").innerHTML = "Falsches Passwort!";
     }
+
+  } else {
+    document.getElementById("result").innerHTML = "Level nicht gefunden!";
+  }
+
+}
